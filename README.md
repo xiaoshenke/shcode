c1107bc2e5cf3ca62900bf266703635eba7c6016
dashu <dashu@wuxiansmac.local>
2017-11-16 13:38:40 +0800

    first upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # shcode
 some pieces shell code from work.
+
+#### hackssh.sh                         
+ssh use /etc/hosts file to transform host to real ip,but when we have multi remote host with the same hostname,we have to edit hosts file everytime. so i write this sh file to hack ssh using anothoer file ,e.g /etc/hosts_finance to loopup host-ip pair.
+you can then user alias command to alia this sh to let'ssay ssh2 for easy using.
+
---
+++ hackssh.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+	echo Usage:hackssh aaa@bbbb
+	exit 2
+fi
+
+#eval aaa@bbb to wholehost
+wholehost=$1
+
+#host pattern
+HOSTPATTERN="@[a-zA-Z0-9_\-\.]+$"
+
+#if pattern not matched,exit
+if ! [[ "$wholehost"=~$HOSTPATTERN ]] 
+then 
+	#echo Pattern matched
+	echo Pattern match fail
+	exit 2
+fi
+
+#eval host from wholehost
+host=${wholehost#*@}
+
+#finance machine hosts path
+hostfilepath="/etc/hosts_finance"
+
+#read ips from hosts file
+ips=`cat $hostfilepath | grep $host`
+
+#if no host find in the file,return
+if [ ! -n "$ips" ]
+then
+	echo host not find!
+	exit 2
+fi
+
+#eval actual ip
+ip=${ips%$host*}
+
+#exec ssh command
+ssh ${wholehost%@*}@$ip
