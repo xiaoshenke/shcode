0895c2c872c8397ebad96e41cd721a31136e223d
xiaoshenke <1205914137@qq.com>
2018-01-19 17:19:39 +0800

    zhihu_img add -o param

--- zhihu_img.sh
+++ zhihu_img.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#Usage: zhihu_img.sh fromURL for eg. zhihu_img.sh https://zhuanlan.zhihu.com/p/19902052
+#Usage: zhihu_img.sh [-o -open] fromURL for eg. zhihu_img.sh https://zhuanlan.zhihu.com/p/19902052
 
-#Todo: not download all images,filter duplicate images
-
-# 0:success 1:false
-# those url which ends with "_r.jpg" is valid
+# 0:success 1:false those url which ends with "_r.jpg" is valid
 function is_valid_url {
 	if [ $# -ne 1 ]
 	then 
@@ -13,7 +10,6 @@ function is_valid_url {
 	name=$1
 	#get rid of .jpg
 	name=${name%%.jpg*}
-
 	#get last 2 chars	
 	if [[ ${name:0-2:2} == "_r" ]]
 	then 
@@ -23,9 +19,22 @@ function is_valid_url {
 	fi
 }
 
+# init open_image flag ->  0:not open 1:open
+open_image=0
+while [ -n "$1" ]
+do
+	case "$1" in
+	-o | -O | -open | -Open) 
+		open_image=1 ;;
+	*) 
+		break;;
+	esac
+	shift
+done 
+
 if [ $# -ne 1 ] 
 then
-	echo Usage:./zhihu_img.sh fromURL
+	echo Usage:./zhihu_img.sh [-o -open] fromURL
 	exit 2
 fi
 
@@ -36,7 +45,6 @@ urls=`gawk 'BEGIN{RS=";"} /https?:[0-9picFu\\\]*.zhimg.com/{print $0}' tmp.html`
 for url in $urls:
 do
 #	new_url=${url[@]//u002F//} `echo $url | sed 's!u002F!/!g'` --> Fixme: slash and backslash replacement....
-	#echo origin url: $url
 	new_url=$url
 	new_url=${new_url%\\\&quot*}
 	new_url=${new_url##*com\\u002F}	
@@ -44,7 +52,7 @@ do
 	is_valid_url $new_url
 	if [ $? -ne 0 ]
 	then
-		echo $new_url not valid,continue
+		#echo $new_url not valid,continue
 		continue
 	fi
 
@@ -52,11 +60,12 @@ do
 	echo begin to download: $new_url
 
 	savefile=${new_url:0-15:15}
-	curl -s -o $savefile $new_url > /dev/null 2>&1 
-	open -a Preview $savefile
+	curl -s -o $savefile $new_url > /dev/null 2>&1
+	if [ $open_image -eq 1  ]
+	then
+		#echo open -a Preview $savefile
+		open -a Preview $savefile
+	fi
 done
 
 rm -f tmp.html
-
-
-
