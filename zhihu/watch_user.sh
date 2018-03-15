e31f88fe96c4b5b0953ad4a5adec3000834bf0e8
xiaoshenke <1205914137@qq.com>
2018-03-15 11:39:02 +0800

    add what_is_new.sh and watch_user.sh

---
+++ zhihu/watch_user.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+# Usage: watch_user.sh user:[activity|answer|post|zhuanlan]
+# for example ./watch_user.sh chihuowushuang:activity default is activity
+
+# implement: save watched user to file:.watched,every day 9:00 13:20 17:50 20:00 23:00 to check whether anyone of then has new updates,save what's new to file:.news,then once ./what_is_new.sh is called,read and clear .news file
+
+
+
