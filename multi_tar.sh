c0d42ac3d2f5ed4e8087149eb369c51d0e3d1c3f
xiaoshenke <1205914137@qq.com>
2019-07-18 00:13:50 +0800

    update multi_tar.sh

--- multi_tar.sh
+++ multi_tar.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 # Used to tar xvzf multi file to destination
-# for eg, find *|grep xyz|./multi_tar.sh to-file
+# for eg, find *|grep xyz|./multi_tar.sh to_file,to_file should endswith .tar.gz 
+# dir name will be to_file remote .tar.gz,for ig,whole.tar.gz =>(generate dir) whole
 
 if [ $# -ne 1 ]
 then
-        echo Usage: find *|grep xyz|./multi_tar.sh to-file
+        echo Usage: find *|grep xyz|./multi_tar.sh to_file[.tar.gz]
         exit 2
 fi
 
+to_file=$1
+# extract file name from input to_file
+name=${to_file%%.tar.gz}
+
+# if file name valid,if condition will be False
+if [ ${#to_file} -eq ${#name} ]
+then
+	echo to_file should be endswith .tar.gz,check your input!!!
+	exit 2
+fi
+
 # step1: check whether .tmp dir exist
-TMP_DIR="m_tar"
+TMP_DIR=$name
 if [ -e $TMP_DIR ] && [ -d $TMP_DIR ]
 then
 	echo $TMP_DIR exist!!! you should clear the dir first to call this sh!
